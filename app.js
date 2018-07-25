f266d17804fd1766e807a5f7e64075e3239e0437
muskangarg12 <muskangargnsit@gmail.com>
2018-07-25 19:54:20 +0530

    app file modified

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
-var password = "entrepreneur";
+var password = process.env.PASSWORD ; // make your own password
 
 //connect to mongo
 mongo.connect('mongodb://admin:admin12@ds129811.mlab.com:29811/mydb', function(err, client){
